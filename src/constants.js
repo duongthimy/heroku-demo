89ad6bad5b320ffa4e8b2cfbf2448d5110e13a34
unknown <my.duong@gooddata.com>
2019-11-11 11:39:44 +0700

    second commit

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// module.exports is important for setupProxy.js
+module.exports = {
+    appName: "My App",
+    backend: "https://zebroids.intgdc.com",
+    projectId: "pwlmfx6b8cx0r4u5w4623o2dijug30p6",
+};
